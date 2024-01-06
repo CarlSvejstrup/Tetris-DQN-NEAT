6fe68ec2d4bac579ec5bff32db2e04f6da6050d8
CarlSvejstrup <carlsvejstrup@gmail.com>
2024-01-06 22:16:17 +0100

    Visualization

--- src/tetris_engine.py
+++ src/tetris_engine.py
@@ -3,18 +3,19 @@ import cv2 as cv
 import random
 
 shapes = {
-    'T': [(0, 0), (-1, 0), (1, 0), (0, -1)],
-    'J': [(0, 0), (-1, 0), (0, -1), (0, -2)],
-    'L': [(0, 0), (1, 0), (0, -1), (0, -2)],
-    'Z': [(0, 0), (-1, 0), (0, -1), (1, -1)],
-    'S': [(0, 0), (-1, -1), (0, -1), (1, 0)],
-    'I': [(0, 0), (0, -1), (0, -2), (0, -3)],
-    'O': [(0, 0), (0, -1), (-1, 0), (-1, -1)],
+    "T": [(0, 0), (-1, 0), (1, 0), (0, -1)],
+    "J": [(0, 0), (-1, 0), (0, -1), (0, -2)],
+    "L": [(0, 0), (1, 0), (0, -1), (0, -2)],
+    "Z": [(0, 0), (-1, 0), (0, -1), (1, -1)],
+    "S": [(0, 0), (-1, -1), (0, -1), (1, 0)],
+    "I": [(0, 0), (0, -1), (0, -2), (0, -3)],
+    "O": [(0, 0), (0, -1), (-1, 0), (-1, -1)],
 }
-shape_names = ['T', 'J', 'L', 'Z', 'S', 'I', 'O']
+shape_names = ["T", "J", "L", "Z", "S", "I", "O"]
 green = (156, 204, 101)
 black = (0, 0, 0)
 white = (255, 255, 255)
+
 
 def rotated(shape):
     return [(-j, i) for i, j in shape]
@@ -29,9 +30,15 @@ def is_occupied(shape, anchor, board):
             return True
     return False
 
+
 def soft_drop(shape, anchor, board):
     new_anchor = (anchor[0], anchor[1] + 1)
-    return (shape, anchor) if is_occupied(shape, new_anchor, board) else (shape, new_anchor)
+    return (
+        (shape, anchor)
+        if is_occupied(shape, new_anchor, board)
+        else (shape, new_anchor)
+    )
+
 
 def hard_drop(shape, anchor, board):
     while True:
@@ -40,11 +47,13 @@ def hard_drop(shape, anchor, board):
             return shape, anchor_new
         anchor = anchor_new
 
+
 class Tetris:
     def __init__(self, width, height):
         self.width = width
         self.height = height
         self.board = np.zeros(shape=(width, height), dtype=np.float64)
+        self.render_enabled = True
 
         # State size (Clearede lines, bumpiness, holes, height)
         self.state_size = 4
@@ -61,11 +70,14 @@ class Tetris:
         self.reset()
 
     def _choose_shape(self):
-
         max_count = max(self._shape_counts)
 
         tetromino = None
-        valid_tetrominos = [shape_names[i] for i in range(len(shapes)) if self._shape_counts[i] < max_count]
+        valid_tetrominos = [
+            shape_names[i]
+            for i in range(len(shapes))
+            if self._shape_counts[i] < max_count
+        ]
         if len(valid_tetrominos) == 0:
             tetromino = random.sample(shape_names, 1)[0]
         else:
@@ -88,7 +100,7 @@ class Tetris:
             if not can_clear[i]:
                 new_board[:, j] = self.board[:, i]
                 j -= 1
-        self.score += sum(can_clear)    
+        self.score += sum(can_clear)
         self.board = new_board
 
         return sum(can_clear)
@@ -114,10 +126,10 @@ class Tetris:
 
         reward = 0
         done = False
-        
+
         self._set_piece(True)
         cleared_lines = self._clear_lines()
-        reward += cleared_lines ** 2 * self.width + 1
+        reward += cleared_lines**2 * self.width + 1
         if np.any(self.board[:, 0]):
             self.reset()
             done = True
@@ -150,7 +162,7 @@ class Tetris:
             if not can_clear[i]:
                 new_board[:, j] = self.board[:, i]
                 j -= 1
-        self.score += sum(can_clear)    
+        self.score += sum(can_clear)
         board = new_board
 
         return sum(can_clear), board
@@ -159,13 +171,13 @@ class Tetris:
         bumpiness = 0
         columns_height = [0 for _ in range(self.width)]
 
-        for i in range(self.width): 
+        for i in range(self.width):
             for j in range(self.height):
                 if board.T[j][i]:
                     columns_height[i] = self.height - j
                     break
         for i in range(1, len(columns_height)):
-            bumpiness += abs(columns_height[i] - columns_height[i-1])
+            bumpiness += abs(columns_height[i] - columns_height[i - 1])
 
         return bumpiness, sum(columns_height)
     
@@ -176,7 +188,7 @@ class Tetris:
             row = 0
             while row < self.height and col[row] == 0:
                 row += 1
-            holes += len([x for x in col[row + 1:] if x == 0])
+            holes += len([x for x in col[row + 1 :] if x == 0])
 
         return holes
 
@@ -191,7 +203,6 @@ class Tetris:
         bumpiness, height = self.get_bumpiness_height(board)
         
         return np.array([cleared_lines, holes, bumpiness, height])
-
 
     def get_next_states(self):
         """To get all possible state from current shape"""
@@ -219,28 +230,46 @@ class Tetris:
             self.shape = rotated(self.shape)
         return states
 
+    def toggle_render(self):
+        self.render_enabled = not self.render_enabled
+
     def render(self, score):
-        self._set_piece(True)
-        board = self.board[:].T
-        board = [[green if board[i][j] else black for j in range(self.width)] for i in range(self.height)]
-        self._set_piece(False)
+        if self.render_enabled:
+            self._set_piece(True)
+            board = self.board[:].T
+            board = [
+                [green if board[i][j] else black for j in range(self.width)]
+                for i in range(self.height)
+            ]
+            self._set_piece(False)
 
-        img = np.array(board).reshape((self.height, self.width, 3)).astype(np.uint8)
-        img = cv.resize(img, (self.width * 25, self.height * 25), interpolation=cv.INTER_NEAREST)
+            img = np.array(board).reshape((self.height, self.width, 3)).astype(np.uint8)
+            img = cv.resize(
+                img, (self.width * 25, self.height * 25), interpolation=cv.INTER_NEAREST
+            )
 
-        # To draw lines every 25 pixels
-        img[[i * 25 for i in range(self.height)], :, :] = 0
-        img[:, [i * 25 for i in range(self.width)], :] = 0
+            # To draw lines every 25 pixels
+            img[[i * 25 for i in range(self.height)], :, :] = 0
+            img[:, [i * 25 for i in range(self.width)], :] = 0
 
-        # Add extra spaces on the top to display game score
-        extra_spaces = np.zeros((2 * 25, self.width * 25, 3))
-        cv.putText(extra_spaces, "Score: " + str(score), (15, 35), cv.FONT_HERSHEY_SIMPLEX, 1, white, 2, cv.LINE_AA)
+            # Add extra spaces on the top to display game score
+            extra_spaces = np.zeros((2 * 25, self.width * 25, 3))
+            cv.putText(
+                extra_spaces,
+                "Score: " + str(score),
+                (15, 35),
+                cv.FONT_HERSHEY_SIMPLEX,
+                1,
+                white,
+                2,
+                cv.LINE_AA,
+            )
 
-        # Add extra spaces to the board image
-        img = np.concatenate((extra_spaces, img), axis=0)
+            # Add extra spaces to the board image
+            img = np.concatenate((extra_spaces, img), axis=0)
 
-        # Draw horizontal line to separate board and extra space area
-        img[50, :, :] = white
+            # Draw horizontal line to separate board and extra space area
+            img[50, :, :] = white
 
-        cv.imshow('DQN Tetris', img)
-        cv.waitKey(1)
+            cv.imshow("DQN Tetris", img)
+            cv.waitKey(1)
