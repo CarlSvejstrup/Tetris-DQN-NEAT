d5d98a0829f555bafad94fa110d8d91f9765b293
CarlSvejstrup <carlsvejstrup@gmail.com>
2024-01-12 10:09:02 +0100

    change in setup

--- submitGPU.sh
+++ submitGPU.sh
@@ -1,6 +1,6 @@
-#BSUB -J jobnam
-#BSUB -o jobname%J.out
-#BSUB -e jobname%J.err
+#BSUB -J test_1
+#BSUB -o test_1%J.out
+#BSUB -e test_1_err%J.err
 #BSUB -n 4
 #BSUB -q gpuv100
 #BSUB -gpu "num=1:mode=exclusive_process"
