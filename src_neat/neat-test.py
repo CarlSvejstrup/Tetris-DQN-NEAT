65a09d7ba4b67ebe6afaddbf20c2795ab87e1dcc
NikolajHJ <nikolaj@jakobsen.us>
2024-01-19 10:28:03 +0100

    commented out irrelevant text

--- src_neat/neat-test.py
+++ src_neat/neat-test.py
@@ -22,14 +22,9 @@ config = neat.Config(neat.DefaultGenome,
 with open("neat_best.pickle", "rb") as f:
     winner = pickle.load(f)
 
-node_names = {-4: 'cleared_lines', -3: 'holes', -2: 'bumpiness', -1: 'height', 0: 'score'}
 winner_net = neat.nn.FeedForwardNetwork.create(winner, config)
-visualize.draw_net(config, winner, True, node_names=node_names)
-
-visualize.plot_stats(stats, ylog=False, view=True)
-
-visualize.plot_species(stats, view=True)
-exit()
+#node_names = {-4: 'cleared_lines', -3: 'holes', -2: 'bumpiness', -1: 'height', 0: 'score'}
+#visualize.draw_net(config, winner, True, node_names=node_names)
 for i in range(times_to_repeat):
     if make_video:
         # Set the video output settings
